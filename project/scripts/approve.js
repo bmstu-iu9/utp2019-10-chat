de1f51369509eaccd28b19f3defec6c089e2c194
dron <ya.andreysavo@gmail.con>
2019-07-06 19:33:35 +0300

    Register update

---
+++ project/scripts/approve.js
@@ -0,0 +1,66 @@
+'use strict'
+const fs = require('fs')
+const core = require('./core')
+const consts = require('./consts')
+const path = require('path')
+const users = '/users.json'
+const unconfirmed =  '/unconfirmed.json'
+
+const approve = (request, response, data) => {
+    let hash = core.getQueryParams(data).hash
+    fs.readFile(path.join(consts.SERVER_PATH, unconfirmed), 'utf-8', (err, objects) => {
+        if (err) {
+            console.log("JSON with user data not found, lol")
+            core.sendError(response, err)
+        }
+        
+        let array = JSON.parse(objects)
+        let findedUser = array.filter(user => {
+            return user.hash === hash
+        }).find(user => true)      
+
+        if (findedUser == undefined){
+            //<-----------------Тут путь к странице с не успешным подтверждением регистрации ------------------------>
+            core.redirect(response, "/failed.html")
+        }
+        else{
+            array = array.filter(user => user != findedUser)
+
+            fs.writeFile(path.join(consts.SERVER_PATH, unconfirmed), JSON.stringify(array), 'utf8', (err) => {
+                if (err) {
+                    console.log("JSON with user data not found, lol")
+                    core.sendError(response, err)
+                }
+            })
+            approveUser(findedUser)
+            //<-----------------Тут путь к странице с успешным подтверждением регистрации ------------------------>
+            core.redirect(response, "/passed.html")
+        }
+    })
+}
+
+const approveUser = (findedUser) => {
+    fs.readFile(path.join(consts.SERVER_PATH, users), 'utf-8', (err, objects) => {
+        if (err) {
+            console.log("JSON with user data not found, lol")
+            core.sendError(response, err)
+        }
+        
+        let array = JSON.parse(objects)
+        array.push({
+            username: findedUser.username,
+            email : findedUser.email,
+            password : findedUser.password
+        })
+
+        fs.writeFile(path.join(consts.SERVER_PATH, users), JSON.stringify(array), 'utf8', (err) => {
+            if (err) {
+                console.log("JSON with user data not found, lol")
+                core.sendError(response, err)
+            }
+        })
+    })
+}
+
+exports.invoke = approve
+
--- project/scripts/registration.js
+++ project/scripts/registration.js
@@ -1,32 +1,78 @@
-const fs = require('fs');
-const core = require('./core');
-const consts = require('./consts');
-const path = require('path');
-
+'use strict'
+const fs = require('fs')
+const core = require('./core')
+const consts = require('./consts')
+const path = require('path')
+const crypto = require('crypto')
+const users = '/users.json'
+const unconfirmed =  '/unconfirmed.json'
+const nodemailer = require('nodemailer')
+            
 const reg = (request, response, data) => {
-    const args = core.getQueryParams(data);
-    fs.readFile(path.join(consts.SERVER_PATH, '/users.json'), 'utf-8', (err, data) => {
+    const args = core.getQueryParams(data)
+    fs.readFile(path.join(consts.SERVER_PATH, users), 'utf-8', (err, data) => {
         if (err) {
-            console.log("JSON with user data not found, lol");
-            core.sendError(response, err);
+            console.log("JSON with user data not found, lol")
+            core.sendError(response, err)
         }
-        let array = JSON.parse(data);
+
+        let array = JSON.parse(data)
         if (array.filter(user => {
-            return user.email == args.email;
+            return user.email == args.email
         }).length !== 0){
-            core.redirect(response, "/failed.html");
+            //<-----------------Тут путь к странице с не успешной регистрацией ------------------------>
+            core.redirect(response, "/failed.html")
         }
         else {
-            array.push(args);
-            fs.writeFile(path.join(consts.SERVER_PATH, '/users.json'), JSON.stringify(array), 'utf8', (err) => {
-                if (err) {
-                    console.log("JSON with user data not found, lol");
-                    core.sendError(response, err);
+            args.hash = crypto.randomBytes(256).toString('hex')
+            let transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: 'QuickChatIU9@gmail.com',
+                    pass: 'BkwUJgLjEP8y2N2'
                 }
-            })
-            core.redirect(response, "/passed.html");
+            });
+
+            let mailOptions = {
+                from: 'QuickChatIU9@gmail.com',
+                to: args.email,
+                subject: 'QuickChat registration!',
+                text: 'Please follow the link below \n\n'+"http://"+ consts.SERVER_IP+"/approve.js?hash="+args.hash
+            };
+
+            transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                    console.log(error);
+                    //<-----------------Тут путь к странице с не успешной регистрацией ------------------------>
+                    core.redirect(response, "/failed.html")
+                } else {
+                    addUserInUnconfirmed(response,args)
+                }
+            });
         }
-    });
+    })
 }
 
-exports.invoke = reg;
+const addUserInUnconfirmed = (response,args) => {
+    fs.readFile(path.join(consts.SERVER_PATH, unconfirmed), 'utf-8', (err, objects) => {
+        if (err) {
+            console.log("JSON with user data not found, lol")
+            core.sendError(response, err)
+        }
+
+        let newConfirmed = JSON.parse(objects)
+        newConfirmed = newConfirmed.filter(user => { return user.email !== args.email})
+        newConfirmed.push(args)
+        fs.writeFile(path.join(consts.SERVER_PATH, unconfirmed ), JSON.stringify(newConfirmed), 'utf-8', (err) => {
+            if (err) {
+                console.log("JSON with user data not found, lol")
+                core.sendError(response, err)
+            }
+        })
+
+        //<----------------Тут путь к странице успешной регистрации----------------------->
+        core.redirect(response, "/passed.html")
+    })
+} 
+
+exports.invoke = reg
