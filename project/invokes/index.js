57f7e9c059135f108cb71b46f20a695e05384110
2wendex2 <2wendex2@mail.ru>
2019-07-22 17:40:15 +0200

    Разделил файлы html фронта и бэка (исправление багов)

--- project/invokes/index.js
+++ project/invokes/index.js
@@ -1,15 +1,15 @@
 'use strict'
-const core = require('../../scripts/core')
+const core = require('../scripts/core')
 const pathModule = require('path')
-const users = require('../../scripts/users')
-const consts = require('../../scripts/consts')
+const users = require('../scripts/users')
+const consts = require('../scripts/consts')
 
 exports.invoke = async (request, response, data) => {
 	const curUser = users.getCurrentUser(request)
-	if (curUser) {
-		core.redirect(response, '/')
+	if (!curUser) {
+		core.redirect(response, '/auth')
 		return
 	}
 	
-	core.sendFullFile(response, pathModule.join(consts.DATA_PATH, 'auth/index.html'))
+	core.sendFullFile(response, pathModule.join(consts.DATA_PATH, 'index.html'))
 }
