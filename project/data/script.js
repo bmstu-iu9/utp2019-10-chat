6155ec9795a32ad0ef693c171db3b0627cdb11d9
amaguma <amaguma5151@gmail.com>
2019-07-28 21:02:31 +0300

    исправил кнопку выхода и профиля

--- project/data/script.js
+++ project/data/script.js
@@ -1,8 +1,13 @@
+const rcodes = {
+    SUCCESS: 0,
+    NOT_AUTHORIZED: 9
+}
+
 const windowSet = document.getElementById('windowSet');
 const profile = document.getElementById('profile');
 const exit = document.getElementById('exit');
 const setting = document.getElementById('setting');
-
+let data = -1;
 
 setting.addEventListener('click', (e) => {
     e.preventDefault();
@@ -10,15 +15,43 @@ setting.addEventListener('click', (e) => {
         windowSet.style.display = "none";
     } else {
         windowSet.style.display = "flex";
-    } 
+    }
 });
 
-profile.addEventListener('click',(e)=>{
-    window.location.href="/profile.html";
+profile.addEventListener('click', (e) => {
+    e.preventDefault();
+    const ereq = new XMLHttpRequest()
+    ereq.open('GET', '/req/exit.js', true)
+    ereq.onreadystatechange = () => {
+        if (ereq.readyState != 4) return;
+        data = JSON.parse(ereq.responseText);
+        switch (data.err) {
+            case rcodes.SUCCESS:
+                window.location.href = "/profile.html";
+                break;
+            case rcodes.NOT_AUTHORIZED:
+                window.location.href = "/error.html";
+                break;
+        }
+    }
+    ereq.send()
 });
 
 exit.addEventListener('click', (e) => {
-    window.location.href="/auth/index.html";
+    e.preventDefault();
+    const ereq = new XMLHttpRequest()
+    ereq.open('GET', '/req/exit.js', true)
+    ereq.onreadystatechange = () => {
+        if (ereq.readyState != 4) return;
+        data = JSON.parse(ereq.responseText);
+        switch (data.err) {
+            case rcodes.SUCCESS:
+                window.location.href = "/auth/index.html";
+                break;
+            case rcodes.NOT_AUTHORIZED:
+                window.location.href = "/error.html";
+                break;
+        }
+    }
+    ereq.send()
 });
-
-
