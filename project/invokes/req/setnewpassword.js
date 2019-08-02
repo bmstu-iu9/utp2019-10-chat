8b4151667d208579605b443f4e86c6445dfc313c
Aliev2000 <alievz2011@gmail.com>
2019-08-02 10:22:15 +0300

    bug fixes

--- project/invokes/req/setnewpassword.js
+++ project/invokes/req/setnewpassword.js
@@ -9,27 +9,31 @@ const jsonfile = require('../../scripts/jsonfile')
 const passwordreset = require('../../scripts/passwordreset')
 
 exports.invoke = async (request, response, data) => {
-	let args
 	try {
-		args = JSON.parse(data)
+		let args
+		try {
+			args = JSON.parse(data)
+		} catch (err) {
+			core.sendJSON(response, {errcode: 'RCODE_JSON_SYNTAX_ERROR', errmessage: 'JSON syntax error'})
+			return
+		}
+
+		if (!args.newPassword) {
+			core.sendJSON(response, {errcode: 'RCODE_INCORRECT_ARGUMENTS', errmessage: 'Incorrect arguments'})
+			return
+		}
+
+		let userEmail = await passwordreset.getMailByHash(args.hash)
+
+		if (!userEmail) {
+			core.sendJSON(response, {errcode: 'RCODE_INCORRECT_HASH', errmessage: 'Incorrect hash'})
+			return
+		}
+
+		await passwordreset.setNewPassword(userEmail, args.hash, args.newPassword)
+
+		core.sendJSON(response, {errcode: null})
 	} catch (err) {
-		core.sendJSON(response, {errcode: 'RCODE_JSON_SYNTAX_ERROR', errmessage: 'JSON syntax error'})
-		return
+		core.sendJSON(response, {errcode: 'RCODE_UNEXPECTED', errmessage: err.toString()})
 	}
-
-	if (!args.newPassword) {
-		core.sendJSON(response, {errcode: 'RCODE_INCORRECT_ARGUMENT', errmessage: 'Incorrect argument'})
-		return
-	}
-
-	let userEmail = await passwordreset.getMailByHash(args.hash)
-
-	if (!userEmail) {
-		core.sendJSON(response, {errcode: 'RCODE_INCORRECT_HASH', errmessage: 'Incorrect hash'})
-		return
-	}
-
-	await passwordreset.setNewPassword(userEmail, args.hash, args.newPassword)
-
-	core.sendJSON(response, {errcode: null})
 }
