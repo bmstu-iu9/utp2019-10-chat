e243b10adabc6bf43e452ddbb8a5f5c99e34ee45
amaguma <amaguma5151@gmail.com>
2019-08-03 18:29:12 +0300

    добавил названия чатов и по ним можно переходить

--- project/data/script.js
+++ project/data/script.js
@@ -10,6 +10,8 @@ const setting = document.getElementById('setting');
 const nameChat = document.getElementById('nameChat');
 const nameDialog = document.getElementById('nameDialog');
 const groupChat = document.getElementById('groupChat');
+const topSetting = document.getElementById('topSetting');
+
 let data = -1;
 
 
@@ -76,28 +78,50 @@ socket.on('connect', () => {
     socket.emit('dialogs', {})
 })
 
-socket.on('dialogs', (data) => {
-    let div = document.createElement('div');
-    div.className = 'nameDialog';		
-    div.innerHTML = '';	
-    if (div.innerHTML === '') {
-        groupChat.style.display = 'none';
-    }
+socket.on('dialogs', (data) => { 
+    if (data.dialogs.length === 0) { 
+    groupChat.style.display = 'none'; 
+    } 
+    topSetting.classList.add('not_active');
     for (let i = data.dialogs.length - 1; i >= 0; i--) { 
-        let div2 = div.cloneNode(true); 
-        div2.innerHTML = data.dialogs[i].name; 
-        groupChat.appendChild(div2); 
-        groupChat.parentNode.insertBefore(div2, div.firstChild); 
-        }
-    // data.dialogs.forEach((element) => {
-    //     let div2 = div.cloneNode(true);
-    //     div2.innerHTML = element.name;
-    //     groupChat.appendChild(div2);
-    //     groupChat.parentNode.insertBefore(div2, div.firstChild);
-    // })
+    let div = document.createElement('div'); 
+    div.className = 'nameDialog'; 
+    div.addEventListener('click', (e) => {
+        e.preventDefault();
+        nameChat.textContent = data.dialogs[i].name;
+        topSetting.classList.remove('not_active');
+    });
+    if (div.textContent === '') { 
+    groupChat.style.display = 'none'; 
+    } 
+    div.textContent = data.dialogs[i].name; 
+    groupChat.appendChild(div); 
+    groupChat.parentNode.insertBefore(div, groupChat); 
+    } 
+    });
+
+socket.on('dialog', (data) => {
+    listdialogs.options[listdialogs.options.length] = new Option(data.name, data.id)
+    chat.hidden = false
 });
 
 socket.on('dialog', (data) => {
     listdialogs.options[listdialogs.options.length] = new Option(data.name,data.id)
     chat.hidden = false
-});
+})
+
+socket.on('message', (data) => {
+  if (listdialogs.value == data.dialogId)
+      chatTable.innerHTML += '</td><td>' + data.name +'</td><td>'+'</td><td>'
+          + data.message.replace(/\n/g, '<br>')+'</td><td>' +'</td><td>'+ (new Date(data.date)).toLocaleString() + '</td></tr>'
+})
+
+socket.on('messages', (data) => {
+  if (listdialogs.value == data.dialogId) {
+      chatTable.innerHTML = ''
+      data.messages.forEach((mes) => {
+          chatTable.innerHTML += '</td><td>' + mes.name +'</td><td>'+'</td><td>' +
+              mes.message.replace(/\n/g, '<br>')+'</td><td>' +'</td><td>'+ (new Date(mes.date)).toLocaleString() + '</td></tr>'
+      })
+  }
+})
