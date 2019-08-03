23bb3d2d6ef91101a88fbc7287047cc5df4ac4ad
amaguma <amaguma5151@gmail.com>
2019-08-03 18:42:26 +0300

    Исправил оишбки x2

--- project/data/script.js
+++ project/data/script.js
@@ -78,55 +78,6 @@ socket.on('connect', () => {
     socket.emit('dialogs', {})
 })
 
-<<<<<<< Updated upstream
-socket.on('dialogs', (data) => { 
-    if (data.dialogs.length === 0) { 
-    groupChat.style.display = 'none'; 
-    } 
-    topSetting.classList.add('not_active');
-    for (let i = data.dialogs.length - 1; i >= 0; i--) { 
-    let div = document.createElement('div'); 
-    div.className = 'nameDialog'; 
-    div.addEventListener('click', (e) => {
-        e.preventDefault();
-        nameChat.textContent = data.dialogs[i].name;
-        topSetting.classList.remove('not_active');
-    });
-    if (div.textContent === '') { 
-    groupChat.style.display = 'none'; 
-    } 
-    div.textContent = data.dialogs[i].name; 
-    groupChat.appendChild(div); 
-    groupChat.parentNode.insertBefore(div, groupChat); 
-    } 
-    });
-
-socket.on('dialog', (data) => {
-    listdialogs.options[listdialogs.options.length] = new Option(data.name, data.id)
-    chat.hidden = false
-});
-
-socket.on('dialog', (data) => {
-    listdialogs.options[listdialogs.options.length] = new Option(data.name,data.id)
-    chat.hidden = false
-})
-
-socket.on('message', (data) => {
-  if (listdialogs.value == data.dialogId)
-      chatTable.innerHTML += '</td><td>' + data.name +'</td><td>'+'</td><td>'
-          + data.message.replace(/\n/g, '<br>')+'</td><td>' +'</td><td>'+ (new Date(data.date)).toLocaleString() + '</td></tr>'
-})
-
-socket.on('messages', (data) => {
-  if (listdialogs.value == data.dialogId) {
-      chatTable.innerHTML = ''
-      data.messages.forEach((mes) => {
-          chatTable.innerHTML += '</td><td>' + mes.name +'</td><td>'+'</td><td>' +
-              mes.message.replace(/\n/g, '<br>')+'</td><td>' +'</td><td>'+ (new Date(mes.date)).toLocaleString() + '</td></tr>'
-      })
-  }
-})
-=======
 socket.on('dialogs', (data) => {
     if (data.dialogs.length === 0) {
         groupChat.style.display = 'none';
@@ -148,4 +99,3 @@ socket.on('dialogs', (data) => {
         groupChat.parentNode.insertBefore(div, groupChat);
     }
 });
->>>>>>> Stashed changes
