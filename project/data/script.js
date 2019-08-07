39791aaf4c13ead166e19177a5423e2326ecf1cb
amaguma <amaguma5151@gmail.com>
2019-08-07 23:22:30 +0300

    чат работает

--- project/data/script.js
+++ project/data/script.js
@@ -11,9 +11,14 @@ const nameChat = document.getElementById('nameChat');
 const nameDialog = document.getElementById('nameDialog');
 const groupChat = document.getElementById('groupChat');
 const topSetting = document.getElementById('topSetting');
+const chat = document.getElementById('chat');
+const dialogUserInfo = document.getElementById('dialogUserInfo');
+const out = document.getElementById('out');
+const chatTable = document.getElementById('chatTable');
+const submit = document.getElementById('submit');
 
 let data = -1;
-
+let dialogIdE = null;
 
 setting.addEventListener('click', (e) => {
     e.preventDefault();
@@ -82,14 +87,28 @@ socket.on('dialogs', (data) => {
     if (data.dialogs.length === 0) {
         groupChat.style.display = 'none';
     }
+    const prevDialog = dialogIdE;
+    chat.style.display = 'none';
     topSetting.classList.add('not_active');
+    out.classList.add('not_active');
+    dialogUserInfo.classList.add('not_active');
+    dialogIdE = null;
     for (let i = data.dialogs.length - 1; i >= 0; i--) {
         let div = document.createElement('div');
         div.className = 'nameDialog';
+        div.id = 'dialogInLeft' + data.dialogs[i].id;
         div.addEventListener('click', (e) => {
             e.preventDefault();
+            if (dialogIdE === data.dialogs[i].id) {
+                return;
+            }
             nameChat.textContent = data.dialogs[i].name;
             topSetting.classList.remove('not_active');
+            out.classList.remove('not_active')
+            dialogUserInfo.classList.remove('not_active')
+            chat.style.display = 'flex';
+            dialogIdE = data.dialogs[i].id;
+            socket.emit('messages', {dialogId: dialogIdE});
         });
         if (div.textContent === '') {
             groupChat.style.display = 'none';
@@ -97,5 +116,46 @@ socket.on('dialogs', (data) => {
         div.textContent = data.dialogs[i].name;
         groupChat.appendChild(div);
         groupChat.parentNode.insertBefore(div, groupChat);
+        if (prevDialog === data.dialogs[i].dialogId) {
+            dialogIdE = prevDialog;
+            socket.emit('messages', {dialogId: dialogIdE});
+        }
     }
 });
+
+socket.on('message', (data) => {
+    chatTable.innerHTML += '</td><td>' + data.name + '</td><td>' + '</td><td>' +
+        data.message.replace(/\n/g, '<br>') + '</td><td>' + '</td><td>' + (new Date(data.date)).toLocaleString() + '</td></tr>'
+})
+
+socket.on('messages', (data) => {
+    chatTable.innerHTML = '';
+    data.messages.forEach((mes) => {
+        chatTable.innerHTML += '<tr><td>' + mes.name + '</td><td>' + '</td><td>' +
+            mes.message.replace(/\n/g, '<br>') + '</td><td>' + '</td><td>' + (new Date(mes.date)).toLocaleString() + '</td></tr>';
+    })
+});
+
+socket.on('err', (data) => {
+    alert(JSON.stringify(data));
+})
+
+submit.addEventListener('click', () => {
+    if (messageText.value == '') return
+    socket.emit('message', {
+        dialogId: dialogIdE,
+        message: messageText.value
+    })
+    messageText.value = ''
+});
+
+socket.on('delete', (data) => {
+    alert(data.id);
+    document.getElementById('dialogInLeft' + data.id).remove(); 
+});
+
+out.addEventListener('click', (e) => {
+    e.preventDefault();
+    socket.emit('delete', {dialogId: dialogIdE});
+});
+
