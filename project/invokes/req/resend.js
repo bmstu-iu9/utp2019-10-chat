73c4da1568f1228cc53d3c79a2ab5fbf089b1737
Aliev2000 <alievz2011@gmail.com>
2019-08-30 12:04:27 +0300

    Переотправка почты

---
+++ project/invokes/req/resend.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const core = require('../../scripts/core')
+const users = require('../../scripts/users')
+const unconfirmed = require('../../scripts/unconfirmed')
+const changeemail = require('../../scripts/changeemail')
+const mail = require('../../scripts/mail')
+
+exports.invoke = async (request, response, data) => {
+	try {
+		let username
+
+		try {
+			username = users.getCurrentUser(request)
+		} catch (err) {
+			core.sendJSON(response, {errcode: 'RCODE_NOT_AUTHORIZED', errmessage: 'Not authorized'})
+			return
+		}
+
+		let hash = await unconfirmed.getHashByName(username)
+
+		if (!hash) {
+			core.sendJSON(response, {errcode: 'RCODE_ALREADE_CONFIRMED', errmessage: 'Already confirmed'})
+			return
+		}
+
+		let email = await changeemail.getUserAcceptEmail(username)
+
+		try {
+			await mail.sendMail(email, 'QuickChat registration!',
+				'Please follow the link below \n\n'+"http://"+request.headers.host+"/approve?hash="+hash)
+		} catch (err) {
+			core.sendJSON(response, {errcode: 'RCODE_FAILED_TO_SEND_EMAIL', errmessage: 'Failed to send email'})
+			return
+		}
+	} catch (err) {
+		core.sendJSON(response, {errcode: 'RCODE_UNEXPECTED', errmessage: err.toString()})
+	}
+}
